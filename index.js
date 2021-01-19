c0a208514949bef082815eb008423bd88b15421f
Hari36020 <harikrishna.sangakari@walkingtree.tech>
2021-01-19 17:58:40 +0530

    test func

--- index.js
+++ index.js
@@ -37,6 +37,63 @@ app.use(express.urlencoded());
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+
+
+app.post('/test', function (request, response) {
+    console.log(request.body);
+    // var body = [];
+    // var body = JSON.stringify(request.body);
+    // body.push(request.body.test) ;
+    var file_name = 'items.txt';
+    csvbody = [Object.values(request.body)];
+    // console.log(body);
+    console.log(csvbody);
+    stringify(csvbody, function (err, data) {
+        console.log(data);
+        var params = {
+            Bucket: 'aws-bucket-2021',
+            Key: file_name,
+            Body: data
+        };
+
+        var options = { partSize: 10 * 1024 * 1024, queueSize: 1 };
+
+        s3.upload(params, options, function (s3err, result) {
+            if (s3err) {
+                //handle the error
+                console.log('s3 error');
+                console.log(s3err);
+            }
+            else {
+                console.log('s3 done in ');
+                //Upload successful. You can delete the S3 files
+                var pg_query = "copy test1 from 's3://aws-bucket-2021/items.txt' " + "ACCESS_KEY_ID 'AKIAY3VA26JR7LH4FG5O' SECRET_ACCESS_KEY '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' escape delimiter as ',';";
+
+                redshiftClient.query(pg_query, { raw: true }, function (err1, pgres) {
+                    //query completed, we can close the connection
+                    // release();
+                    if (err1) {
+                        console.log('error here');
+                        console.error(err1);
+                        response.send('error');
+                    } else {
+                        //upload successful
+                        console.log('upload success');
+                        response.send('success');
+
+                    }
+                });
+                //  redshiftClient.close(); 
+                console.log('outside');
+            }
+        });
+    });
+
+}
+);
+
+//original
+
 app.post('/', function (request, response) {
     console.log(request.body);
     // var body = [];
@@ -87,4 +144,5 @@ app.post('/', function (request, response) {
         });
     });
 
-});
+}
+);
